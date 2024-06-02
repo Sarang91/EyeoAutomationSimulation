e33da645e76d42631efb9af93d05d5f763d9a0b4
sarang <sarang@sarang-VMware-Virtual-Platform>
2024-06-02 18:56:34 +0100

    Added ChromeOption.

--- src/base/BaseClass.java
+++ src/base/BaseClass.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.util.*;
@@ -24,7 +25,9 @@ public class BaseClass
 	
 	public static WebDriver instantiateDriver()
 	{
-		WebDriver driver = new ChromeDriver();
+		ChromeOptions co = new ChromeOptions();
+		co.addArguments("--no-sandbox");
+		WebDriver driver = new ChromeDriver(co);
 		return driver;
 	}
 	
